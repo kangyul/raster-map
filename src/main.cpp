cafad280c59cd930ad5ab74e07672c1eacc2f423
Yul Kang <yulkang2019@gmail.com>
2026-08-27 22:17:41 +0900

    Draw a quad with an element buffer

--- src/main.cpp
+++ src/main.cpp
@@ -66,9 +66,15 @@ int main() {
   std::cout << glGetString(GL_VERSION) << '\n';
 
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f,  -0.5f, 0.0f,
-    0.0f,  0.5f,  0.0f
+     0.5f,   0.5f,  0.0f, // top-right
+     0.5f,  -0.5f,  0.0f, // bottom-right
+    -0.5f,  -0.5f,  0.0f, // bottom-left
+    -0.5f,  0.5f, 0.0f  // top-left
+  };
+
+  unsigned int indices[] = {
+    0, 1, 3, // first-triangle 
+    1, 2, 3 // second-triangle
   };
 
   unsigned int vertexShader;
@@ -93,13 +99,17 @@ int main() {
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  unsigned int VBO, VAO;
+  unsigned int VBO, VAO, EBO;
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
   glGenVertexArrays(1, &VAO);
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
@@ -111,7 +121,7 @@ int main() {
     
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
     
     glfwSwapBuffers(window);
     glfwPollEvents();
